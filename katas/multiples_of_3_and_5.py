ad00bf6ba12b69e2a21656cb557e1a06a102703c
Javier García Valdepeñas <jgvaldepenas@idealista.com>
2022-11-05 18:52:32 +0100

    Add solution

--- katas/multiples_of_3_and_5.py
+++ katas/multiples_of_3_and_5.py
@@ -1,2 +1,6 @@
 def multiples_of_3_and_5(number):
-    pass
+    multiples_of_3_and_5 = []
+    for digit in range(number):
+        if digit % 3 == 0 or digit % 5 == 0:
+            multiples_of_3_and_5.append(digit)
+    return sum(multiples_of_3_and_5)
