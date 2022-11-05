b3d7df6f9ce0b7e3a190cb8508900a4a58bf74ee
Javier García Valdepeñas <jgvaldepenas@idealista.com>
2022-11-05 23:06:16 +0100

    Add final solution

--- katas/snail.py
+++ katas/snail.py
@@ -12,27 +12,29 @@ def obtain_row(snail_map):
 def obtain_column(snail_map):
     result = []
     rows = len(snail_map)
-    columns = len(snail_map[0])
     for row in range(rows):
+        columns = len(snail_map[row])
         result.append(snail_map[row][columns - 1])
         del snail_map[row][columns - 1]
     return result
 
 
 def obtain_row_reversed(snail_map):
-    rows = len(snail_map) - 1
-    result = snail_map[rows]
-    del snail_map[rows]
-    return reversed(result)
+    if snail_map:
+        rows = len(snail_map) - 1
+        result = snail_map[rows]
+        del snail_map[rows]
+        return reversed(result)
+    return snail_map
 
 
 def obtain_column_reversed(snail_map):
     result = []
     rows = len(snail_map)
-    for row in range(rows):
+    for row in reversed(range(rows)):
         result.append(snail_map[row][0])
         del snail_map[row][0]
-    return reversed(result)
+    return result
 
 
 def snail(snail_map):
@@ -41,11 +43,10 @@ def snail(snail_map):
 
     result = []
 
-    while len(snail_map) > 1:
+    while len(snail_map) > 0:
         result.extend(obtain_row(snail_map))
         result.extend(obtain_column(snail_map))
         result.extend(obtain_row_reversed(snail_map))
         result.extend(obtain_column_reversed(snail_map))
 
-    result.append(snail_map[0][0])
     return result
