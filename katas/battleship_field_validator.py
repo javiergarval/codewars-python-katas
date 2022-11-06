8437e8962dc649232fb41902b2b7bb68dceae42a
Javier García Valdepeñas <jgvaldepenas@idealista.com>
2022-11-06 11:09:17 +0100

    Add battleship field validator kata and tests that fail

---
+++ katas/battleship_field_validator.py
@@ -0,0 +1,5 @@
+"""
+Kata: https://www.codewars.com/kata/52bb6539a4cf1b12d90005b7/train/python
+"""
+def battleship_field_validator(field):
+    pass
---
+++ tests/test_battleship_field_validator.py
@@ -0,0 +1,18 @@
+import unittest
+
+from katas.battleship_field_validator import battleship_field_validator
+
+
+class Test(unittest.TestCase):
+    def test_battleship_field_validator(self):
+        battleField = [[1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+                       [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+                       [1, 0, 1, 0, 1, 1, 1, 0, 1, 0],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                       [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                       [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+        self.assertTrue(battleship_field_validator(battleField))
