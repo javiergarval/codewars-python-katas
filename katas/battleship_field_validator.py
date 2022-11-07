089a8c5a074672b1079d787615a8920b1a734137
Javier García Valdepeñas <jgvaldepenas@idealista.com>
2022-11-07 15:55:14 +0100

    Add battleship field validator kata solution

--- katas/battleship_field_validator.py
+++ katas/battleship_field_validator.py
@@ -17,60 +17,66 @@ def categorize_ship():
 
     if counter == 1:
         if submarine_count > 4:
-            raise ValueError
+            return False
         submarine_count += 1
     if counter == 2:
         if destroyer_count > 3:
-            raise ValueError
+            return False
         destroyer_count += 1
     if counter == 3:
         if cruiser_count > 2:
-            raise ValueError
+            return False
         cruiser_count += 1
     if counter == 4:
         if battleship_count > 1:
-            raise ValueError
+            return False
         battleship_count += 1
+
+    return True
 
 
 def find_ship(row, column, field):
     global counter
 
     field[row][column] = 2
-    counter += 1
-
-    if column + 1 < 10 and field[row][column + 1]:
-        for i in range(1, 4):
-            if column + i < 10 and field[row][column + i]:  # Horizontal
-                counter += 1
-                field[row][column + i] = 2
-                if field[row + 1][column + i]:
-                    raise ValueError
-
-    if row + 1 < 10 and field[row + 1][column]:
-        for i in range(1, 4):
-            if row + i < 10 and field[row + i][column]:  # Vertical
-                counter += 1
-                field[row + i][column] = 2
-                if field[row + i][column + 1]:
-                    raise ValueError
-
-    categorize_ship()
 
     counter = 0
+    for i in range(0, 4):
+        if continues_horizontally(row, column + i, field):
+            counter += 1
+            field[row][column + i] = 2
+            if continues_vertically(row + 1, column + i, field):
+                return False
+
+        if continues_vertically(row + i, column, field):
+            counter += 1
+            field[row + i][column] = 2
+            if continues_horizontally(row + i, column + 1, field):
+                return False
+
+    if not categorize_ship():
+        return False
+
+    counter = 0
+    return True
+
+
+def continues_horizontally(row, column, field):
+    return column < 10 and field[row][column] == 1
+
+
+def continues_vertically(row, column, field):
+    return row < 10 and field[row][column] == 1
 
 
 def validate_battlefield(field):
     if not len(field) == 10 and not len(field[0]) == 10:
         return False
 
+    result = False
     for row in range(10):
         for column in range(10):
             if not field[row][column] == 1:
                 continue
-            try:
-                find_ship(row, column, field)
-            except ValueError:
-                return False
-
-    return True
+            result = find_ship(row, column, field)
+    return result and battleship_count == 1 and cruiser_count == 2 and destroyer_count == 3 and submarine_count == 4
