2aadc1a08ba29c5f0a8d11cb27e00acbb80382ff
Javier García Valdepeñas <jgvaldepenas@idealista.com>
2022-11-11 07:22:02 +0100

    Add tests

--- katas/evaluate_mathematical_expression.py
+++ katas/evaluate_mathematical_expression.py
@@ -2,6 +2,18 @@
 Kata: https://www.codewars.com/kata/52a78825cdfc2cfc87000005/train/python
 """
 
+MULTIPLY_OPERATOR = '*'
+DIVIDE_OPERATOR = '/'
+ADD_OPERATOR = '+'
+SUBTRACT_OPERATOR = '-'
+OPENING_PARENTHESIS = '('
+CLOSING_PARENTHESIS = ')'
 
-def evaluate_mathematical_expression(expression):
-    pass
+opening_parenthesis_counter = 0
+closing_parenthesis_counter = 0
+
+
+def evaluate_mathematical_expression(expression: str):
+    for char in expression:
+        pass
+
